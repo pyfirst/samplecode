61c22f10076e7b6df31f2a2e310a9b9bced341c1
Takanori Suzuki <takanory@beproud.jp>
2017-04-26 10:32:25 +0900

    add request for next url and theme, subtheme and rating

--- 4_scraping/lego_scraper/lego_scraper/spiders/brickset.py
+++ 4_scraping/lego_scraper/lego_scraper/spiders/brickset.py
@@ -11,10 +11,16 @@ class BricksetSpider(scrapy.Spider):
         for set in response.css('article.set'):
             meta = set.css('div.meta')
             # セット番号を取得
-            number = meta.css('h1 span::text').re(r'(.+): ')[0]
+            number = meta.css('h1 span::text').re_first(r'(.+): ')
             # セット名を取得
             name = set.css('div.highslide-caption h1 a::text').extract_first()
             yield {
                 'number': number,
                 'name': name,
+                'theme': meta.css('.tags a')[1].css('a::text').extract_first(),
+                'subtheme': meta.css('.tags a.subtheme::text').extract_first(),
+                'rating': meta.css('.rating::attr("title")').extract_first(),
             }
+        next_url = response.css('li.next a::attr("href")').extract_first()
+        if next_url:
+            yield scrapy.Request(next_url)
