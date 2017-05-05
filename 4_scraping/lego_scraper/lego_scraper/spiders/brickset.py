bdabde4d961860a95dceaa4086d86efd2b8ed91a
Takanori Suzuki <takanori@takanory.net>
2017-05-05 12:45:29 +0900

    fix 'More' issue https://gitlab.com/takanory/pymook/issues/28

--- 4_scraping/lego_scraper/lego_scraper/spiders/brickset.py
+++ 4_scraping/lego_scraper/lego_scraper/spiders/brickset.py
@@ -19,8 +19,6 @@ class BricksetSpider(scrapy.Spider):
             # セット名を取得
             name = brickset.css('div.highslide-caption h1 a::text').extract_first()
             price = meta.xpath('.//dt[text()="RRP"]/following-sibling::dd/text()')
-            us_price = price.re_first('\$([\d+\.\d+]+)')
-            eu_price = price.re_first('([\d+\.\d+]+)€')
             yield {
                 'number': number,
                 'name': name,
@@ -29,10 +27,10 @@ class BricksetSpider(scrapy.Spider):
                 'subtheme': meta.css('.tags a.subtheme::text').extract_first(),
                 'year': meta.css('a.year::text').extract_first(),
                 'rating': meta.css('.rating::attr(title)').extract_first(),
-                'pieces': meta.xpath('.//dt[text()="Pieces"]/following-sibling::dd/a/text()').extract_first(),
-                'minifigs': meta.xpath('.//dt[text()="Minifigs"]/following-sibling::dd/a/text()').extract_first(),
-                'us_price': us_price,
-                'eu_price': eu_price,
+                'pieces': meta.xpath('.//dt[text()="Pieces"]/following-sibling::dd').css('::text').extract_first(),
+                'minifigs': meta.xpath('.//dt[text()="Minifigs"]/following-sibling::dd').css('::text').extract_first(),
+                'us_price': price.re_first('\$([\d+\.\d+]+)'),
+                'eu_price': price.re_first('([\d+\.\d+]+)€'),
                 'owner': brickset.css('dl.admin dd').re_first('(\d+) own this set'),
                 'want_it': brickset.css('dl.admin dd').re_first('(\d+) want it'),
             }
