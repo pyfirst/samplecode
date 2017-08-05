52473b7b9ee39e67789f802fb0f6380de9e32996
Hiroki KIYOHARA <hirokiky@gmail.com>
2017-08-05 10:00:06 +0900

    Added chapter 2

---
+++ 2_guide/fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return 'FizzBuzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
+    else:
+        return str(num)
+
+
+for num in range(1, 101):
+    print(fizzbuzz(num))
